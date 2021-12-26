1b88b67779515feb96d8a6fdacc60a07fba63d34
sarmn <meatballsym@gmail.com>
2021-12-27 01:44:51 +0800

    add exten

---
+++ application/extensions/crawler/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin python3
+# -*- coding: utf-8 -*-
+
+# @Author: shangyameng
+# @Email: shangyameng@aliyun.com
+# @Date: 2020-04-11 20:32:42
+# @LastEditTime: 2020-04-11 20:34:24
+# @FilePath: /crawler/__init__.py
+
+from .dytt import DyHeavenCrawler
+from .proxy import ProxyCrawler
+from .zeroMag import ZeroCrawler
+
+
+class TaskFunctions(object):
+    pass
+
